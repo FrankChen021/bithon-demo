bc31e889a7c942a6368c347bd59834556698c081
frank chen <frank.chen021@outlook.com>
2022-03-13 16:37:39 +0800

    Add start up script

---
+++ docker/start-docker-compose.sh
@@ -0,0 +1,4 @@
+# NOTE: the `pull` option makes sure we're using the latest image(https://hub.docker.com/r/bithon/server/tags) of Bithon server
+cd docker && docker-compose pull bithon-server
+cd ..
+docker-compose -f docker/docker-compose.yml up
